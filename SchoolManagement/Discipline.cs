ecdaebe9c7487f9666bbb07fcc610cb81da8a646
Carl Svahn <calle_tag@hotmail.com>
2019-11-12 15:14:49 +0100

    Added fields to class Discipline

---
+++ SchoolManagement/Discipline.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SchoolManagement
+{
+    public class Discipline
+    {
+        private int lectures;
+        private int exercises;
+        private int subject;
+    }
+}
